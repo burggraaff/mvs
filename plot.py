20a008e3d10a20c035df3546863dac2156bae4f4
Olivier Burggraaff <burggraaff@pczaal1.strw.leidenuniv.nl>
2017-02-19 18:02:27 +0100

    Plot GLS in both modes; documentation

--- plot.py
+++ plot.py
@@ -10,6 +10,18 @@ from matplotlib.collections import PatchCollection
 from .tables import convert_gls_array_to_table
 
 def save_show(saveto = None, fig = None, **kwargs):
+    """
+    Save a plot or show it
+
+    Parameters
+    ----------
+    saveto: str, optional
+        Path to save to. If None, show the figure instead.
+    fig: matplotlib.figure.Figure
+        Figure to save/show. If None, use API instead (plt.show rather than fig.show)
+    **kwargs:
+        keyword arguments for show/savefig
+    """
     if saveto is None:
         if fig is None:
             plt.show(**kwargs)
@@ -61,77 +73,55 @@ def make_ylim(y, mag = True, zoom = False):
         ylim = (ymin, ymax)
     return ylim
 
-def lspplot(pLSP, bestp = None, retax = False, axkw = {}, fkw = {}, lkw = {}):
+def gls_one(GLS, y_linlog = "log", x_linlog = "log", highlight = None, title = "Generalised Lomb-Scargle Periodogram", grid = True, axes_kwargs = {}, line_kwargs = {}, **figure_kwargs):
     """
-    Plot a Lomb-Scargle Periodogram on linear and logarithmic scales.
+    Plot a given GLS
 
     Parameters
     ----------
-    pLSP: astropy.table.Table
-        table with period and LSP power
-    bestp: float, optional
-        the best period in the LSP
-    retax: bool, optional
-        whether or not to return the axes objects
-    axkw: dict, optional
-        **kwargs for both the axes objects
-    fkw: dict, optional
-        **kwargs for the main figure
-    lkw: dict, optional
-        **kwargs for the plotted lines
+    GLS: astropy.table.table.Table
+        GLS to plot. It is assumed to have "period" and "power" columns.
+        If you want to plot a GLS in a different format, please use the mvs.plot.gls wrapper function
+    y_linlog: str, optional
+        If "log", plot on a logarithmic y-scale. If "lin", linear.
+    x_linlog: str, optional
+        If "log", plot on a logarithmic x-scale. If "lin", linear.
+    highlight: float, optional
+        Highlight a certain area with a red square.
+        Default: None
+    title: str, optional
+        Title for the plot
+        Default: "Generalised Lomb-Scargle Periodogram"
+    grid: boolean, optional
+        Whether or not to add a grid to the plot
+        Default: True
+    axes_kwargs: dict, optional
+        Keyword arguments for the axes object.
+        e.g. xlabel, ylabel, xlim, ylim, ...
+        Default: {}
+    line_kwargs: dict, optional
+        Keyword arguments for the line object.
+        e.g. ls, lw, color, ...
+        Default: {}
+    **figure_kwargs:
+        Keyword arguments for the figure object.
+        e.g. tight_layout, figsize, ...
 
     Returns
     -------
     fig: matplotlib.figure.Figure
-        the created figure
-    axs: list of matplotlib.axes.AxesSubplot, optional
-        the axes objects of plots themselves
+        Figure containing the plot
+    ax: matplotlib.axes._axes.Axes
+        Axes object containing the plot
     """
-    fkwA = {"title": "Lomb-Scargle Periodogram"}
-    combinekwargs(fkwA, fkw)
-
-    axkwA = {"xlim": (pLSP["p"].min(), pLSP["p"].max()), "xscale": "log", "ylabel": "Power"}
-    combinekwargs(axkwA, axkw)
-
-    lkwA = {"color": "black", "lw": '1', "label": "Lomb-Scargle Periodogram"}
-    combinekwargs(lkwA, lkw)
-
-    fig, axs = plt.subplots(nrows = 2, sharex = True, figsize=figsize, subplot_kw = axkwA)
-
-    axs[0].tick_params(axis='x', labelbottom="off")
-    axs[1].tick_params(axis='x', which="both", direction="out", length=10, top="off")
-    axs[1].set_xlabel("Period (days)")
-
-    for ax in axs:
-        ax.plot(pLSP["p"], pLSP["Power"], **lkwA)
-        if bestp is not None:
-            ax.axvspan(0.9*bestp, 1.1*bestp, color="r", alpha=0.5, zorder=0)
-
-        ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-
-        ax.grid(axis='x', which="major", color=lkwA["color"], ls="--")
-        ax.grid(axis='y', which="major", color=lkwA["color"], ls="-.")
-
-    axs[0].set_title(fkwA["title"])
-
-    axs[1].set_yscale("log")
-
-    maxx = pLSP["Power"].max()
-    axs[0].set_ylim(0, 1.03*maxx)
-    axs[1].set_ylim(10.**-5., 1.)
-
-    fig.tight_layout()
-
-    return returnwhich([fig, axs], [True, retax])
-
-def gls_one(GLS, y_linlog = "log", x_linlog = "log", highlight = None, title = "Generalised Lomb-Scargle Periodogram", grid = True, axes_kwargs = {}, line_kwargs = {}, **figure_kwargs):
     f_kw = {"tight_layout": True, "figsize": (20, 15)}
     f_kw.update(figure_kwargs)
 
     x_linlog = "linear" if x_linlog == "lin" else "log"
     y_linlog = "linear" if y_linlog == "lin" else "log"
     default_ylim = (0, 1) if y_linlog == "linear" else (1e-5, 1)
-    ax_kw = {"xlabel": "Period (days)", "ylabel": "Power", "title": title, "xscale": x_linlog, "yscale": y_linlog, "ylim": default_ylim}
+    default_xlim = (GLS["period"].min(), GLS["period"].max())
+    ax_kw = {"xlabel": "Period (days)", "ylabel": "Power", "title": title, "xscale": x_linlog, "yscale": y_linlog, "ylim": default_ylim, "xlim": default_xlim}
     ax_kw.update(axes_kwargs)
 
     l_kw  = {"color": "black", "lw": '1'}
@@ -149,15 +139,124 @@ def gls_one(GLS, y_linlog = "log", x_linlog = "log", highlight = None, title = "
 
     return fig, ax
 
-def gls_both(GLS):
-    pass
+def gls_both(GLS, x_linlog = "log", highlight = None, title = "Generalised Lomb-Scargle Periodogram", grid = True, ylims=((0, 1), (1e-5, 1)), axes_kwargs = {}, line_kwargs = {}, **figure_kwargs):
+    """
+    Plot a given GLS with linear *and* logarithmic y axes
 
-def gls(GLS, y_linlog = "log", saveto = None, save_kwargs = {}, **kwargs):
-    y_linlog = y_linlog.lower()
-    assert y_linlog in ("lin", "log", "both"), "mvs.plot.gls expects a value of `lin`, `log`, or `both` for the parameter `y_linlog`; instead got `{0}`".format(y_linlog)
+    Parameters
+    ----------
+    GLS: astropy.table.table.Table
+        GLS to plot. It is assumed to have "period" and "power" columns.
+        If you want to plot a GLS in a different format, please use the mvs.plot.gls wrapper function
+    x_linlog: str, optional
+        If "log", plot on a logarithmic x-scale. If "lin", linear.
+    highlight: float, optional
+        Highlight a certain area with a red square.
+        Default: None
+    title: str, optional
+        Title for the plot
+        Default: "Generalised Lomb-Scargle Periodogram"
+    grid: boolean, optional
+        Whether or not to add a grid to the plot
+        Default: True
+    ylims: tuple, optional
+        Limits on the vertical axes of the plots
+        ((lin_min, lin_max), (log_min, log_max))
+        Default: ((0, 1), (1e-5, 1))
+    axes_kwargs: dict, optional
+        Keyword arguments for the axes object.
+        e.g. xlabel, ylabel, xlim, ylim, ...
+        Default: {}
+    line_kwargs: dict, optional
+        Keyword arguments for the line object.
+        e.g. ls, lw, color, ...
+        Default: {}
+    **figure_kwargs:
+        Keyword arguments for the figure object.
+        e.g. tight_layout, figsize, ...
+
+    Returns
+    -------
+    fig: matplotlib.figure.Figure
+        Figure containing the plots
+    axs: list of matplotlib.axes._axes.Axes
+        Axes objects containing the plots
+    """
+    f_kw = {"tight_layout": True, "figsize": (20, 15), "sharex": True}
+    f_kw.update(figure_kwargs)
+
+    l_kw  = {"color": "black", "lw": "1"}
+    l_kw.update(line_kwargs)
+
+    default_xlim = (GLS["period"].min(), GLS["period"].max())
+    ax_kw = {"xlabel": "Period (days)", "ylabel": "Power", "xscale": x_linlog, "xlim": default_xlim}
+    ax_kw.update(axes_kwargs)
+    xlabel = ax_kw.pop("xlabel") # only want this on the lower plot
+
+    fig, axs = plt.subplots(2, subplot_kw = ax_kw, **f_kw)
+    axs[0].tick_params(axis='x', labelbottom="off") # no labels on horizontal axis for top plot
+    axs[1].tick_params(axis='x', which="both", direction="out", length=10, top="off")
+    axs[1].set_xlabel(xlabel)
+
+    for ax in axs:
+        ax.plot(GLS["period"], GLS["power"], **l_kw)
+        if highlight is not None:
+            ax.axvspan(0.9 * highlight, 1.1 * highlight, color = "red", alpha = 0.5, zorder = 0)
+        if grid:
+            ax.grid(axis='x', which="major", color="black", ls="-.")
+            ax.grid(axis='y', which="major", color="black", ls="-.")
+
+    axs[0].set_title(title)
+    axs[1].set_yscale("log")
+    axs[0].set_ylim(ylims[0])
+    axs[1].set_ylim(ylims[1])
+
+    return fig, axs
+
+def gls(GLS, mode = "log", saveto = None, save_kwargs = {}, return_fig_axs = False, **kwargs):
+    """
+    Wrapper for plotting GLS
+
+    Parameters
+    ----------
+    GLS: astropy.table.table.Table or np.ndarray
+        GLS. If table, assumed to have "period" and "power" columns.
+        If numpy array, it is converted to an astropy table.
+    mode: str, optional
+        If "lin", make one plot with a linear y-axis.
+        If "log", make one plot with a logarithmic y-axis.
+        If "both", make a plot that has both of the above.
+        Default: "log"
+    saveto: str, optional
+        Where to save the figure. Figure is shown if None.
+        Default: None
+    save_kwargs: dict, optional
+        Keyword arguments for savefig/show.
+        Default: {}
+    return_fig_axs: boolean, optional
+        Whether or not to return the figure and axes objects.
+        If False, the figure is closed.
+        Default: False
+    **kwargs:
+        Keyword arguments for mvs.plot.gls_both or mvs.plot.gls_one
+
+    Returns
+    -------
+    Both only if return_fig_axs = True:
+
+    fig: matplotlib.figure.Figure
+        Figure containing the plots
+    axs: (list of) matplotlib.axes._axes.Axes
+        Axes object(s) containing the plot(s)
+    """
+    mode = mode.lower()
+    assert mode in ("lin", "log", "both"), "mvs.plot.gls expects a value of `lin`, `log`, or `both` for the parameter `mode`; instead got `{0}`".format(mode)
     GLS_ = convert_gls_array_to_table(GLS)
-    if y_linlog == "both":
-        fig, axs = gls_both(GLS_, **kwargs)
+    GLS.sort("period")
+    if mode == "both":
+        fig, ax = gls_both(GLS_, **kwargs)
     else:
-        fig, ax = gls_one(GLS_, y_linlog = y_linlog, **kwargs)
+        fig, ax = gls_one(GLS_, y_linlog = mode, **kwargs)
     save_show(saveto, fig, **save_kwargs)
+    if return_fig_axs:
+        return fig, ax
