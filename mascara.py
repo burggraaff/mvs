c778a2486ac714ec409d8942f2ffd9c9ef8575dd
Olivier Burggraaff <olivierburggraaff@gmail.com>
2021-08-17 00:12:39 +0200

    Plot GLS

--- mascara.py
+++ mascara.py
@@ -6,6 +6,8 @@ from matplotlib import pyplot as plt
 from sys import argv
 from pathlib import Path
 from astropy.timeseries import LombScargle as GLS
+from operator import truediv
+from functools import partial
 
 from mvs import io
 
@@ -24,3 +26,21 @@ gls = GLS(data["HJD"], data["mag0"], dy=data["emag0"])
 
 frequencies = np.logspace(-2, 2, 10000)
 power = gls.power(frequencies)
+
+# Plot GLS
+invert = partial(truediv, 1)
+
+plt.figure(figsize=(10,3))
+plt.plot(frequencies, power, c='k', lw=1)
+plt.xlabel("Frequency [d$^{-1}$]")
+plt.ylabel("Power")
+plt.xscale("log")
+plt.yscale("log")
+plt.xlim(frequencies.min(), frequencies.max())
+plt.ylim(1e-5, 1.01)
+secax = plt.gca().secondary_xaxis("top", functions=(invert, invert))
+secax.set_xlabel("Period [d]")
+plt.grid(ls="--")
+plt.title(f"ASCC {ascc}")
+plt.show()
+plt.close()
